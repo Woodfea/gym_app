3dece528f7cfc127e6657a917b6aaba187438c8f
Guillaume COGONI <cogoni.guillaume@gmail.com>
2026-01-29 15:09:30 +0100

    fix: change the response status code when an user is created (200 -> 201)

--- backend/src/main/java/com/gym_app/backend/controllers/AuthController.java
+++ backend/src/main/java/com/gym_app/backend/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.gym_app.backend.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +31,6 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
         userService.register(request.getUsername(), request.getPassword(), request.getEmail(), request.getIcon_path());
-        return ResponseEntity.ok("User created with success !");
+        return new ResponseEntity<String>("User created with success !", HttpStatus.CREATED);
     }
 }
